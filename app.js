791b933cb9d662f11852a4d27acf6a954f9153ab
EgijaG <57557140+EgijaG@users.noreply.github.com>
2021-06-03 00:07:27 +0300

    Left: responsiveness and tooltip

--- app.js
+++ app.js
@@ -5,28 +5,64 @@ const contents = [
     progress: '25%',
     name: 'branding',
     firstRow: 'Company logo',
-    scndRow: 'Logo position'
+    scndRow: 'Logo position',
+    extra: `<div class="form-check">
+      <input class="form-check-input" type="radio" name="options" id="optionOne" value="option1">
+      <label class="form-check-label" for="optionOne">Top left</label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="options" id="optionTwo" value="option1">
+      <label class="form-check-label" for="optionTwo">Centre</label>
+    </div>`
   },
   {
     id: 2,
     progress: '50%',
     name: 'photos',
     firstRow: 'Watermarks',
-    scndRow: 'Aspect ratio'
+    scndRow: 'Aspect ratio',
+    extra: `<div class="form-check">
+      <input class="form-check-input" type="radio" name="options" id="optionOne" value="option1">
+      <label class="form-check-label" for="optionOne">4:2</label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="options" id="optionTwo" value="option1">
+      <label class="form-check-label" for="optionTwo">3:2</label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="options" id="optionThree" value="option1">
+      <label class="form-check-label" for="optionThree">16:9</label>
+    </div>`
   },
   {
     id: 3,
     progress: '75%',
     name: 'floorplans',
     firstRow: 'Total area',
-    scndRow: 'Color'
+    scndRow: 'Color',
+    extra: `<div class="form-check">
+        <input class="form-check-input" type="radio" name="options" id="optionOne" value="option1">
+        <label class="form-check-label" for="optionOne">Colored</label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="options" id="optionTwo" value="option1">
+        <label class="form-check-label" for="optionTwo">Black & white</label>
+      </div>`
   },
   {
     id: 4,
     progress: '99%',
     name: 'tour',
     firstRow: 'Welcome screen',
-    scndRow: 'Welcome screen logo'
+    scndRow:
+      'Welcome screen logo' +
+      `<div class="con-tooltip top">
+          <p>i</p>
+        <div class="tooltip">
+          <p>Can be enabled only if the “Welcome screen” is enabled</p>
+        </div>
+      </div>`,
+    extra: document.querySelector('.switch').innerHTML
   }
 ]
 
@@ -36,7 +72,8 @@ function replaceContentInContainer (target, source) {
   ).innerHTML
 }
 
-function replaceFirst () {
+function replaceOptions () {
+  const template = document.querySelector('template')
   contents.forEach(con => {
     if (counter > 4) {
       replaceContentInContainer('start', 'finished')
@@ -49,6 +86,8 @@ function replaceFirst () {
       document.querySelector('.progress-bar').style.width = con.progress
       document.querySelector('.subhead-title').innerHTML = con.name
       document.getElementById('firstRow').innerHTML = con.firstRow
+      document.querySelector('.right').appendChild(template.content, true)
+      document.querySelector('.extra').innerHTML = con.extra
       document.getElementById('scndRow').innerHTML = con.scndRow
     }
   })
